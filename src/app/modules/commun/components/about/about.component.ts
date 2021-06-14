20bbdeadee952c8d80e821a9777ee18e2415901f
Soham Dahiya <54260201+sdMickey@users.noreply.github.com>
2021-06-14 16:35:37 +0530

    final commit

---
+++ src/app/modules/commun/components/about/about.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:
+  {
+    apiKey: "AIzaSyAimCtAbTnZoYYbTOdt_ny3nZRJp2BdLXs",
+    authDomain: "cafe-10.firebaseapp.com",
+    databaseURL: "https://cafe-10-default-rtdb.firebaseio.com",
+    projectId: "cafe-10",
+    storageBucket: "cafe-10.appspot.com",
+    messagingSenderId: "471447623211",
+    appId: "1:471447623211:web:ab0affe62d99483870fc6e",
+    measurementId: "G-SMDKPMB4J5"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
